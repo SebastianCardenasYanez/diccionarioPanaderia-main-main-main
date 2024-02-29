829a24d9ce944a675a77778a05c26d7fdeb216cc
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-29 14:17:26 -0500

    comando no lee parentecis

--- panaderia.py
+++ panaderia.py
@@ -13,8 +13,8 @@ menu = dict({
             {"nombre": "Pan de bocadillo", "valor": 800},
         ]),
         "Promociones1": list([
-            {"codigo" :8, "nombre": "compre 10", "valor": 25000},
-            {"codigo" :5, "nombre": "compre 5", "valor": 8000},
+            {"codigo" :8, "Pan de centeno": "compre 10 unidades", "valor": 25000},
+            {"codigo" :5, "Rosca de maiz": "compre 5 unidades", "valor": 8000},
             ])
     },
     "Pasteles": {
@@ -31,8 +31,8 @@ menu = dict({
             {"nombre": "Milhojas", "valor": 1500},
         ]),
         "Promociones2": list([
-            {"codigo" :6, "nombre": "compre 3", "valor": 3500},
-            {"codigo" :9, "nombre": "compre 5", "valor": 6500},
+            {"codigo" :6, "Empanada de frutas": "compre 3 unidades", "valor": 3500},
+            {"codigo" :9, "Milhojas": "compre 5 unidades", "valor": 6500},
         ])
     },
     "Galletas": {
@@ -49,8 +49,8 @@ menu = dict({
             {"nombre": "Galletas de jengibre", "valor": 1500},
         ]),
         "Promociones3": list([
-            {"codigo":9, "nombre": "compre 5", "valor": 4000},
-            {"codigo":0, "nombre": "compre 4", "valor": 5000},
+            {"codigo":9, "Galletas de jengibre": "compre 5 unidades", "valor": 4000},
+            {"codigo":0, "Galletas de avena": "compre 4 unidades", "valor": 5000},
         ])
     }
 })
@@ -62,16 +62,69 @@ for i, val in enumerate(listaCategoria):
 opcioncategoria = int(input())
 
 if opcioncategoria == 0:
+    datosCategoria = (menu.get(listaCategoria)[opcioncategoria])
+    valorProducto = (datosCategoria.get("Producto").get("valor"))
     #print(menu.get("Pan").get("Producto"))
     print("seleccione el producto : ")
     for i, val in enumerate(menu.get("Pan").get("Producto")):
         print(f"{i}. {val}")
     opcionProducto = int(input())
+    if opcionProducto == 8 :
+        print(f"Hay una promocion de {menu.get("Pan").get("Promociones1")[0]}")
+        deseaPromocion = int(input("¿Usuario desea la promocion? "))
+        for i in enumerate("si", "no") :
+            if deseaPromocion == 0 :
+                valorPromocion1 = int(25000)
+                print(f"seria un total de {valorPromocion1}") 
+                dinero = int(input("Ingrese su dinero :"))
+            if dinero >= valorUnidades :
+                vueltos = dinero - valorUnidades
+                print (f"sus vueltos son {vueltos}")
+            elif dinero < valorUnidades :
+                print ("no tiene dinero suficiente, gracias por su compra")
+            elif deseaPromocion == 1 :
+                cantUnidades = int(input(f"cuantas unidades desea llevar del producto elegido"))
+            valorUnidades = cantUnidades * valorProducto
+            print(f"Seria un total de {cantUnidades} por un precio de {valorUnidades}")
+            dinero = int(input("Ingrese su dinero :"))
+            if dinero >= valorUnidades :
+                vueltos = dinero - valorUnidades
+                print (f"sus vueltos son {vueltos}")
+            elif dinero < valorUnidades :
+                print ("no tiene dinero suficiente, gracias por su compra")
+    elif opcionProducto == 5 :
+        print(f"Hay una promocion de {menu.get("Pan").get("Promociones1")[1]}")
+        deseaPromocion = int(input("¿Usuario desea la promocion? "))
+        for i in enumerate("si", "no") :
+            if deseaPromocion == 0 :
+                valorPromocion1 = int(8000)
+                print(f"seria un total de {valorPromocion1}") 
+                dinero = int(input("Ingrese su dinero :"))
+            if dinero >= valorUnidades :
+                vueltos = dinero - valorUnidades
+                print (f"sus vueltos son {vueltos}")
+            elif dinero < valorUnidades :
+                print ("no tiene dinero suficiente, gracias por su compra")
+            elif deseaPromocion == 1 :
+                cantUnidades = int(input(f"cuantas unidades desea llevar del producto elegido"))
+            valorUnidades = cantUnidades * valorProducto
+            print(f"Seria un total de {cantUnidades} por un precio de {valorUnidades}")
+            dinero = int(input("Ingrese su dinero :"))
+            if dinero >= valorUnidades :
+                vueltos = dinero - valorUnidades
+                print (f"sus vueltos son {vueltos}")
+            elif dinero < valorUnidades :
+                print ("no tiene dinero suficiente, gracias por su compra")
+
+
+
+
 elif opcioncategoria == 1:
     print("seleccione el producto : ")
     for i, val in enumerate(menu.get("Pasteles").get("Productos")):
         print(f"{i}. {val}")
     opcionProducto = int(input())
+
 elif opcioncategoria == 2:
     print("seleccione el producto : ")
     for i, val in enumerate(menu.get("Galletas").get("Productoss")):
@@ -83,7 +136,8 @@ elif opcioncategoria == 2:
 
 
 
-print(menu[opcioncategorias]["Productos"])
+
+
 
 
     
